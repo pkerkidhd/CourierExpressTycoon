66492213b1c15d723edaced9228452738d0f2bfe
David Vo <pkerkidhd@gmail.com>
2014-05-04 16:05:54 -0400

    -> Created Building Functions

---
+++ UnityGame/Assets/Scripts/Building/BuildingPlacement.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingPlacement : MonoBehaviour {
+
+	private PlaceableBuilding placeableBuilding;
+	private Transform currentBuilding;
+	private bool hasPlaced;
+	private PlaceableBuilding placeableBuildingOld;
+
+	public LayerMask buildingsMask;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 m = Input.mousePosition;
+		m = new Vector3(m.x, m.y, transform.position.y);
+		Vector3 p = camera.ScreenToWorldPoint(m);
+
+		if (currentBuilding != null && !hasPlaced) {
+			currentBuilding.position = new Vector3(p.x, 0.2f, p.z);
+
+			if (Input.GetMouseButtonDown(0)) {
+				if(IsLeagalPosition()) {
+					hasPlaced = true;
+				}
+			}
+		} else {
+			if (Input.GetMouseButtonDown(0)) {
+				RaycastHit hit = new RaycastHit();
+				Ray ray = new Ray(new Vector3(p.x, 9, p.z), Vector3.down);
+				if (Physics.Raycast(ray, out hit, Mathf.Infinity, buildingsMask)) {
+					if(placeableBuildingOld != null) {
+						placeableBuildingOld.SetSelected(false);
+					}
+					hit.collider.gameObject.GetComponent<PlaceableBuilding>().SetSelected(true);
+					placeableBuildingOld = hit.collider.gameObject.GetComponent<PlaceableBuilding>();
+				} else {
+					if(placeableBuildingOld != null) {
+						placeableBuildingOld.SetSelected(false);
+					}
+				}
+			}
+		}
+	}
+
+	bool IsLeagalPosition() {
+		if (placeableBuilding.colliders.Count > 0) {
+			return false;
+		}
+		return true;
+	}
+
+	public void SetItem(GameObject b) {
+		hasPlaced = false;
+		currentBuilding = ((GameObject)Instantiate(b)).transform;
+		placeableBuilding = currentBuilding.GetComponent<PlaceableBuilding>();
+	}
+}
---
+++ UnityGame/Assets/Scripts/Player/CameraMovement.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public int boundary = 20;
+	public int speed = 15;
+	
+	public int theScreenWidth;
+	public int theScreenHeight;
+	
+	public float xPos;
+	public float yPos;
+	public float zPos;
+	
+	// Use this for initialization
+	void Start () {
+		theScreenWidth = Screen.width;
+		theScreenHeight = Screen.height;
+		
+		xPos = transform.position.x;
+		yPos = transform.position.y;
+		zPos = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.position = new Vector3(xPos, yPos, zPos);
+		
+		if (Input.GetMouseButtonDown(0)) {
+		//	Debug.Log("Mouse pos X: " + Input.mousePosition.x + " Y: " + Input.mousePosition.y + " Z: " + Input.mousePosition.z);
+		//	Debug.Log("Camera pos X: " + transform.position.x + " Y: " + transform.position.y + " Z: " + transform.position.z);
+		}
+		
+		if (Input.GetAxis("Mouse ScrollWheel") > 0) {
+			if (transform.position.y > 4) {
+				yPos -= 10.0f * Time.deltaTime;
+				//Debug.Log("Y: " + transform.position.y);
+			}
+		}
+		
+		if (Input.GetAxis("Mouse ScrollWheel") < 0) {
+			if (transform.position.y < 9) {
+				yPos += 10.0f * Time.deltaTime;
+				//Debug.Log("Y: " + transform.position.y);
+			}
+		}
+		
+		if (Input.GetKey(KeyCode.Space)) {
+			//yPos = 36.41333f;
+		}
+		
+		if (Input.mousePosition.x > theScreenWidth - boundary) {
+		//	if (transform.position.x < 188) {
+				xPos += speed * Time.deltaTime;
+				//Debug.Log("X: " + transform.position.x);
+		//	}
+		}
+		
+		if (Input.mousePosition.x < 0 + boundary)
+		{
+		//	if (transform.position.x > 9.5) {
+				xPos -= speed * Time.deltaTime;
+				//Debug.Log("X: " + transform.position.x);
+		//	}
+		} 
+		
+		if (Input.mousePosition.y > theScreenHeight - boundary)
+		{
+		//	if (transform.position.z < 84) {
+				zPos += speed * Time.deltaTime;
+				//Debug.Log("Z: " + transform.position.z);
+		//	}
+		}
+		
+		if (Input.mousePosition.y < 0 + boundary)
+		{
+		//	if (transform.position.z > 11) {
+				zPos -= speed * Time.deltaTime;
+				//Debug.Log("Z: " + transform.position.z);
+		//	}
+		}
+		
+	}
+}
