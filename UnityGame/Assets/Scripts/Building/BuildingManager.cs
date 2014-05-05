41f3de5e7f7606764c6bd84dd863a96b52d60729
David Vo <pkerkidhd@gmail.com>
2014-05-05 15:37:07 -0400

    <- Replace Plane with Terrian

--- UnityGame/Assets/Scripts/Building/BuildingManager.cs
+++ UnityGame/Assets/Scripts/Building/BuildingManager.cs
@@ -7,7 +7,7 @@ public class BuildingManager : MonoBehaviour {
 	private BuildingPlacement buildingPlacement;
 	private Economy_HUD ecoHUD;
 
-	private float buildingPrice = 25;
+	private float buildingPrice = 0;
 	private float newBalance = 0;
 
 	// Use this for initialization
@@ -22,13 +22,16 @@ public class BuildingManager : MonoBehaviour {
 	}
 
 	void OnGUI () {
-		for (int i = 0; i < buildings.Length; i++) {
-			if (GUI.Button(new Rect(Screen.width/20,Screen.height/15 + Screen.height/12 * i, 100, 30), buildings[i].name)) {
+		int i = 0;
+		foreach (GameObject go in buildings) {
+			buildingPrice = go.gameObject.GetComponent<BuildingCost>().Cost;
+			if (GUI.Button(new Rect(Screen.width/20,Screen.height/15 + Screen.height/12 * i, 100, 30), go.gameObject.name)) {
 				newBalance = ecoHUD.playerMoney - buildingPrice;
 				if (newBalance >= 0) {
-					buildingPlacement.SetItem(buildings[i], buildingPrice);
+					buildingPlacement.SetItem(go.gameObject, buildingPrice);
 				}
 			}
+			i++;
 		}
 	}
 }
